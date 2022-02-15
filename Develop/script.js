3e04bfa74f7267c51195eab21c71b897c2e940c1
megan <mtpott23@gmail.com>
2022-02-15 15:32:52 -0500

    finish prompt functions for character selections

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,15 @@
 // Assignment code here
 
-// when i CLICK the button to generate a password
-var promptPassword = function() {
-
-  var promptClick = window.prompt("How many characters would you like to use in your password? You may choose between 8 and 128 characters.");
-    //if number entered is less than 8 or greater than 128, inform user that their number is not in range
-    if (promptClick < 8 || promptClick > 128) {
-      window.alert("Your number is out of our selected range! Please try again");
-      //return the function to let the user try again
-      return promptPassword;
-    } 
-}
+// // when i CLICK the button to generate a password
+// var generatePassword = function() {
+//   var promptClick = window.prompt("How many characters would you like to use in your password? You may choose between 8 and 128 characters.");
+//     //if number entered is less than 8 or greater than 128, inform user that their number is not in range
+//     if (promptClick < 8 || promptClick > 128) {
+//       window.alert("Your number is out of our selected range! Please try again");
+//       //return the function to let the user try again
+//       return generatePassword;
+//     } 
+// }
 
 
 // i am presented with a series of PROMPTS for password criteria
@@ -28,17 +27,78 @@ var promptPassword = function() {
 // the password is either displayed in an ALERT or written to the page
 
 
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
+    function generatePassword(){
+    var promptClick = window.prompt("How many characters would you like to use in your password? You may choose between 8 and 128 characters. Please enter your choice below.");
+    //if number entered is less than 8 or greater than 128, inform user that their number is not in range
+    if (promptClick < 8 || promptClick > 128) {
+        window.alert("Your number is out of our selected range! Please choose again.");
+        //return the function to let the user try again
+        return generatePassword();
+    }
+    //i confirm whether or not to include lowercase (true/false)
+    var lowCaseConfirm = window.confirm("Would you like to use lowercase characters?");
+    //if true, inform user they decided to use lowercase characters and return true
+    if (lowCaseConfirm) {
+        window.alert("You have decided to use lowercase characters.");
+        return true;
+    };
+    //confirm uppercase characters (true/false)
+    var upCaseConfirm = window.confirm("Would you like to use uppercase characters?");
+      if (upCaseConfirm) {
+      //if true, inform user they decided to use uppercase characters and return true
+          window.alert("You have decided to use uppercase characters.");
+          return true;
+      };
+    //confirm numeric values (true/false)
+    var numConfirm = window.confirm("Would you like to use numeric values?");
+      if(numConfirm) {
+      //if true, inform user they decided to use numeric values and return true
+        window.alert("You have decided to use numeric values.");
+        return true;
+      };
+    //confirm special characters (true/false)
+    var charConfirm = window.confirm("Would you like to use special characters?");
+    if (charConfirm) {
+    //if true, inform user they decided to use special characters and return true
+        window.alert("You have decided to use special characters.");
+        return true;
+      };
+    }    
+}
+
+  //using the selections above, use the function to create the password with Math.random()
+  function getRandomPassword() {
+    const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const special = " !#$%&'()*+,-./:;,=>?@[\]^_`{|}~";
+    const num = "1234567890";
+    const len = 0;
+
+    //use a for loop and Math.random() to generate a random string
+    for (var i = 0; i < len; i++) {
+        var randomString = Math.floor(Math.random() * alphabet.length * special.length * num.length);
+        return randomString;
+    }
+  }
+
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//create password!  
+writePassword();
+  
